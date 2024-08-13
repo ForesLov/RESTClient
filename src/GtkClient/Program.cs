c53d778011e78c8155421d2459b974cf3e906575
Katy248 <petrovanton247@gmail.com>
2024-08-13 16:09:28 +0300

    Add ui example

--- src/GtkClient/Program.cs
+++ src/GtkClient/Program.cs
@@ -3,7 +3,17 @@ using Gio;
 
 const string AppId = "";
 var app = Adw.Application.New(AppId, ApplicationFlags.DefaultFlags);
-var window = ApplicationWindow.New();
+
+var window = new ApplicationWindow
+{
+    DefaultWidth = 600,
+};
+
+var view = new OverlaySplitView();
+view.SetMinSidebarWidth(200);
+
+window.SetContent(view);
+
 app.OnActivate += (s, args) => {
   app.AddWindow(window);
   window.Present();
