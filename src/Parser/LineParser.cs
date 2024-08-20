6562c06ef849589649844cb15dc5f65e0946e572
ForesLov <fores.clerc@gmail.com>
2024-08-20 16:44:44 +0300

    Добавлен класс обработки строки и вычленения данных в ней

---
+++ src/Parser/LineParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient.Parser;
+
+public class LineParser
+{
+    private string Line;
+    public LineParser(string line) 
+    { 
+        Line = line;
+    }
+    public List <string> comments = new List <string>();
+    public List <string> requestData = new List <string>();
+    public string requestInterrupt = "";
+    void Parse(string Line )
+    {
+        if (string.IsNullOrEmpty(Line))
+        {
+            if (Line.StartsWith("#") || Line.StartsWith("//"))
+            {
+                comments.Add(Line);
+            }
+            else if(Line.StartsWith("###")) 
+            {
+                requestInterrupt = Line;
+            }
+            else
+            {
+                requestData.Add(Line);
+            }
+        }
+    }
+}
---
+++ src/Parser/StringContent.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient.Parser
+{
+    public class StringContent
+    {
+        private string _request;
+        public StringContent(string Request)
+        {
+            _request = Request;
+        }
+
+        public List<string[]> HTTPRequests = new List<string[]>(); 
+        
+        public void RequestParse()
+        {
+            if (string.IsNullOrEmpty(_request)) return;
+            string[] Lines = _request.Split(new char[] { '\n' });
+            int count = 0;
+            for (int i = count; i < Lines.Length-1; count++)
+            {
+                RequestAdd(Lines, count);
+            }
+        }
+        private void RequestAdd(string[] Lines, int count)
+        {
+            string Comments ="";
+            string Body="";
+            string[] Arr = new string[2];
+            string[] localLicnes = new string[Lines.Length-count-1];
+            for (int i = count; i < localLicnes.Length-1; i++)
+            {
+                localLicnes[i] = Lines[i];
+            }
+            foreach (string Line in localLicnes)
+            {
+                if (!string.IsNullOrEmpty(Line))
+                {
+                    string[] RequestBody = Line.Split(new char[] { '#' });
+                    if (!string.IsNullOrEmpty(RequestBody[0]))
+                    {
+                        Body += RequestBody[0] + "\n";
+                        if (!string.IsNullOrEmpty(RequestBody[1]))
+                            Comments += RequestBody[1] + "\n";
+                    }
+                    if (Line == "###")
+                    {
+                        Comments += Line;
+                        Arr[0] =  Body;
+                        Arr[1] = Comments;
+                        break;
+                    }
+                }
+                count++;
+            }
+           HTTPRequests.Add(Arr);
+        }
+    }
+}
