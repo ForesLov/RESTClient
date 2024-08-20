b4d6c24b7b8d410c6241e64fd479ce4b30a8ff0e
ForesLov <fores.clerc@gmail.com>
2024-08-20 18:32:59 +0300

    Создан класс сканера и метод сканирования директорий на заданное количество вложенности

---
+++ src/Parser/RequestScaner.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestClient.Parser
+{
+    public class RequestScaner
+    {
+        string path;
+        public RequestScaner(string Path) 
+        {
+            path = Path;
+        }
+        public string[] files;
+        
+        public void Scan(int level = 2)
+        {
+            if (Directory.Exists(path))
+            {
+                files = (string[])Directory.GetFiles(path, "*.http").Where(f => f.Split(new char[] { '/' }).Count() <= level + path.Split(new char[] { '/' }).Count());
+            }
+        }
+    }
+}
