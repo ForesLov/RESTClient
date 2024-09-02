d29e313928ad5970187f1cbb286057bb0c2beec9
ForesLov <fores.clerc@gmail.com>
2024-09-02 08:36:59 +0300

    Добавлен тест парсера строк (работает, вроде) и тест сканера (сканер не работает). А также добавлена библиотека NUnit (хз зачем)

--- src/Parser.Test/LineParserTest.cs
+++ src/Parser.Test/LineParserTest.cs
@@ -8,6 +8,48 @@ namespace Parser.Test
 {
     public class LineParserTest
     {
-
+        string [] testData = { "//", "#", "GET /index.html HTTP/1.1" };
+        List<string> CommentsControl = new List<string>();
+        List<string> Comments = new List<string>();
+        List<string> BodiesControl = new List<string>();
+        List<string> Bodies = new List<string>();
+        [Fact]
+        void TestBodie()
+        {
+            for (int i = 0; i < testData.Length - 1; i++)
+            {
+                if (i < 2)
+                    CommentsControl.Add(testData[i]);
+                else
+                    BodiesControl.Add(testData[i]);
+            }
+            LineParser lineParser = new LineParser();
+            for (int i = 0;i < testData.Length-1;i++)
+            {
+                lineParser.Parse(testData[i]);
+            }
+            Bodies = lineParser.requestData;
+            
+            Assert.Equal(Bodies, BodiesControl);
+            
+        }
+        [Fact]
+        void TestComments()
+        {
+            for (int i = 0; i < testData.Length - 1; i++)
+            {
+                if (i < 2)
+                    CommentsControl.Add(testData[i]);
+                else
+                    BodiesControl.Add(testData[i]);
+            }
+            LineParser lineParser = new LineParser();
+            for (int i = 0; i < testData.Length - 1; i++)
+            {
+                lineParser.Parse(testData[i]);
+            }
+            Comments = lineParser.comments;
+            Assert.Equal(Comments, CommentsControl);
+        }
     }
 }
---
+++ src/Parser.Test/RequestScanerTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Test
+{
+    public class RequestScanerTest
+    {
+        [Fact]
+        void TestScanner()
+        {
+            string path = "E:\\ForesLov\\RESTClient\\examples";
+            RequestScaner scaner = new RequestScaner(path);
+            string[] files = scaner.Scan();
+            string[] controlData = { "E:\\ForesLov\\RESTClient\\examples\\v1\\some.http", "E:\\ForesLov\\RESTClient\\examples\\v2\\another.http", "E:\\ForesLov\\RESTClient\\examples\\v2\\some.http" };
+
+            Assert.Equal(files, controlData);
+        }
+
+    }
+}
