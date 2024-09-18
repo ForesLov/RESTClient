00d92519fd9c115236bd37fba407454373abf72c
Katy248 <petrovanton247@gmail.com>
2024-09-18 11:57:58 +0300

    Refactor scanner

--- src/Parser/RequestScaner.cs
+++ src/Parser/RequestScaner.cs
@@ -1,40 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RestClient.Parser;
 
-namespace RestClient.Parser
+public class RequestScaner
 {
-    public class RequestScaner
+    private readonly DirectoryInfo _dir;
+    public RequestScaner(DirectoryInfo dir)
     {
-        string path;
-        public RequestScaner(string Path) 
+        _dir = dir;
+    }
+
+    public FileInfo[] Scan(int level = 2)
+    {
+        return (GetFiles(_dir, level - 1));
+    }
+
+    private FileInfo[] GetFiles(DirectoryInfo dir, int depth)
+    {
+        if (!dir.Exists)
+            return [];
+
+        var result = new List<FileInfo>();
+        result.AddRange(dir.GetFiles("*.http", SearchOption.TopDirectoryOnly));
+
+        if (depth > 0)
         {
-            path = Path;
-        }
-        
-        
-        /*public void Scan(int level = 2)
-        {
-            if (Directory.Exists(path))
+            foreach (var subdir in dir.GetDirectories())
             {
-                files = (string[])Directory.GetFiles(path, "*.http").Where(f => f.Split(new char[] { '/' }).Count() <= level + path.Split(new char[] { '/' }).Count());
+                result.AddRange(GetFiles(subdir, depth - 1));
             }
-        }*/
-        public string[] Scan(int level = 2)
-        {
-            List<string> files = new List<string>();
-            if (Directory.Exists(path))
-            {
-                var localPath = Directory.GetDirectories(path).Where(x => x.Split('/').Count() <= level + path.Split('/').Count());
-                foreach (var directory in localPath)
-                {
-                    files.AddRange( Directory.GetFiles(directory, "*.http", SearchOption.TopDirectoryOnly));
-                }
-                return files.ToArray();
-            }
-            return null;
         }
+        return result.ToArray();
+
     }
 }
