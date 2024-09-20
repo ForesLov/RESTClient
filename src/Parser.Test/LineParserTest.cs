1a311a96927c7156911a3399367a275d7dc31a95
Katy248 <petrovanton247@gmail.com>
2024-09-20 15:06:31 +0300

    Small refactor

--- src/Parser.Test/LineParserTest.cs
+++ src/Parser.Test/LineParserTest.cs
@@ -1,31 +1,30 @@
 ﻿using RestClient.Parser.Contracts;
 using RestClient.Parser.Tokens;
 
-namespace Parser.Test
-{
-    public class LineParserTest
-    {
-        [Theory]
-        [InlineData("//")]
-        [InlineData("#")]
-        // [InlineData("###")]
-        void ParseCommentsTest(string value)
-        {
-            var expectedData = new CommentToken(value);
-            LineParser lineParser = new LineParser(value);
-            lineParser.Parse().Should().BeEquivalentTo(expectedData);
-        }
+namespace Parser.Test;
 
-        [Theory]
-        [InlineData("GET /index.html HTTP/1.1")]
-        [InlineData("POST /index.html")]
-        [InlineData("  POST /index.html/with-spaces")]
-        [InlineData("\tPOST /index.html/with-tab")]
-        void ParseRequestDataTest(string requestStr)
-        {
-            IToken controlData = new RequestToken(requestStr);
-            var parsedData = new LineParser(requestStr).Parse();
-            controlData.Should().BeEquivalentTo(controlData);
-        }
+public class LineParserTest
+{
+    [Theory]
+    [InlineData("//")]
+    [InlineData("#")]
+    // [InlineData("###")]
+    void ParseCommentsTest(string value)
+    {
+        var expectedData = new CommentToken(value);
+        LineParser lineParser = new LineParser(value);
+        lineParser.Parse().Should().BeEquivalentTo(expectedData);
+    }
+
+    [Theory]
+    [InlineData("GET /index.html HTTP/1.1")]
+    [InlineData("POST /index.html")]
+    [InlineData("  POST /index.html/with-spaces")]
+    [InlineData("\tPOST /index.html/with-tab")]
+    void ParseRequestDataTest(string requestStr)
+    {
+        IToken controlData = new RequestToken(requestStr);
+        var parsedData = new LineParser(requestStr).Parse();
+        controlData.Should().BeEquivalentTo(controlData);
     }
 }
